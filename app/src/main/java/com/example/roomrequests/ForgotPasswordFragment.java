0afc86b3a204d7950a4524405a3ee727056f97d1
gmitch2 <gamitchell4@gmail.com>
2020-02-11 09:00:51 -0500

    added forgot password dialog

---
+++ app/src/main/java/com/example/roomrequests/ForgotPasswordFragment.java
@@ -0,0 +1,49 @@
+package com.example.roomrequests;
+
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ForgotPasswordFragment extends DialogFragment {
+
+
+    public ForgotPasswordFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setView(R.layout.layout_dialog_forgotpassword);
+        builder.setTitle(R.string.password_reset);
+        builder.setMessage(R.string.forgot_password_dialog)
+                .setPositiveButton(R.string.send, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // FIRE ZE MISSILES!
+                    }
+                })
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                    }
+                });
+        // Create the AlertDialog object and return it
+        return builder.create();
+    }
+
+}
--- app/src/main/java/com/example/roomrequests/LoginActivity.java
+++ app/src/main/java/com/example/roomrequests/LoginActivity.java
@@ -2,13 +2,42 @@ package com.example.roomrequests;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    TextView tvForgot;
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        tvForgot = findViewById(R.id.tv_login_forgot);
+
+        btnLogin = findViewById(R.id.btn_login_submit);
+
+        tvForgot.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                ForgotPasswordFragment dialog = new ForgotPasswordFragment();
+                dialog.show(getSupportFragmentManager(),"Forgot Password");
+                return false;
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
